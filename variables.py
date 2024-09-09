7e98c654dddeca59057c4474a2664ab2b7750e8a
Micah Kennedy <144722193+micahk828@users.noreply.github.com>
2024-09-09 09:29:20 -0400

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,3 +1,13 @@
 fname = "Micah"
 age = 16
 fact = True
+
+print(8)
+print(fname)
+print(age)
+print(fact)
+print("We are the Best")
+
+question = input("What Day Is It")
+
+print("Happy " + question + ", " + fname + "!")
