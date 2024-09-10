07b034f0ca6019d020b87ec7939280b5b43133b1
Micah Kennedy <144722193+micahk828@users.noreply.github.com>
2024-09-10 08:40:21 -0400

    Update and rename doNow.py to 9_10doNow.py

---
+++ 9_10doNow.py
@@ -0,0 +1,16 @@
+fname = input("What is your first name?")
+lname = input("What is your last name?")
+full_name = fname + " " + lname + "!!!"
+
+print("Hey, " + full_name)
+
+fcolor = input("What is your favorite color")
+grade = input("What is your grade level?")
+subject = input("What is your CTE course")
+
+print(" ")
+print("My Profile")
+print(" ")
+print("My favorite color is " + fcolor)
+print("I'm in the " + grade + " grade")
+print("My CTE course is " + subject)
--- doNow.py
+++
@@ -1,5 +0,0 @@
-fname = input("What is your first name?")
-lname = input("What is your last name?")
-full_name = fname + " " + lname + "!!!"
-
-print("Hey, " + full_name)
