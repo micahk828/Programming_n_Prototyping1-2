e74357f7edb8d07d58e3d6b37ab6e32eb4dc2d1e
Micah Kennedy <144722193+micahk828@users.noreply.github.com>
2024-09-13 08:53:14 -0400

    Create catEC.py

---
+++ unit1/catEC.py
@@ -0,0 +1,17 @@
+print("Without Variables\n")
+print(".         .")
+print(" \\'_\"'\"_'/")
+print("  } 6 6 {")
+print("  =. Y .=")
+print("   /^^^\  .")
+print("  /     \  )")
+print("  ( )-( )/")
+print('   "" ""')
+
+
+print("\n\n\n")
+
+print("With Variables\n")
+part1 = ".         .\n"+" \\'_\"'\"_'/\n"+"  } 6 6 {\n"+"  =. Y .=\n"+"   /^^^\  .\n" + "  /     \  )\n" + "  ( )-( )/\n" + '   "" ""'
+print(part1)
+
