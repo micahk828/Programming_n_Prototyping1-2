79c8dabb69aa0e2f82794eb6186f963fa5443380
Micah Kennedy <144722193+micahk828@users.noreply.github.com>
2024-09-16 09:19:21 -0400

    Create CFU2.py

---
+++ unit1/CFU2.py
@@ -0,0 +1,11 @@
+#9/16/2024
+#Micah Kennedy
+
+start = input("Click enter to create your profile")
+fname = input("What is your first name")
+lname = input("What is your last name")
+age = input("How old are you?")
+sleep = input("How many hours do you sleep?")
+
+print(fname + "'s Profile\n\n" + "Name: " + fname, lname + "\nAge: " + age + " years old\n" + "Average Sleep: " + sleep + " hours")
+print("You turn 21 in " + str(21-int(age)) + " years!")
