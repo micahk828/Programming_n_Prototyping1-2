b5fc323bc118e1d680abb943453dc834ded6f72a
Micah Kennedy <144722193+micahk828@users.noreply.github.com>
2024-09-17 09:02:40 -0400

    Create SillySentences.py

---
+++ unit1/SillySentences.py
@@ -0,0 +1,18 @@
+#Micah Kennedy
+#9/17/2024
+#Pd1-2
+
+print("Let's play Silly Sentences!")
+name = input("Enter a name: ")
+adjective1 = input("Enter a adjective: ")
+adjective2 = input("Enter a adjective: ")
+adverb = input("Enter a adverb: ")
+food1 = input("Enter a food: ")
+food2 = input("Enter a food: ")
+noun = input("Enter a noun: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb: ")
+print(name + " was planning a dream vacation to " + place + ".")
+print(name + " was especially looking forward to trying the local cuisine, including " + adjective1,food1 + " and " + food2 + ".")
+print(name + " will have to practice the language " + adverb + " to make it easier to " + verb + " with people.")
+print(name + " has a long list of sights to see, including the " + noun + " museum and the " + adjective2 + " park.")
