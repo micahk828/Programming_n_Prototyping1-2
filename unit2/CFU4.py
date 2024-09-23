cda4b659107bafa8d9d45400ac429decf0e3c330
Micah Kennedy <144722193+micahk828@users.noreply.github.com>
2024-09-23 17:26:52 -0400

    Create CFU4.py

---
+++ unit2/CFU4.py
@@ -0,0 +1,14 @@
+#Micah Kennedy
+#9/23/2024
+#Pd 1-2
+
+import math
+name = input("What is your name?")
+print("Hi, ", name + "!")
+a = int(input("Enter a value for the a coeficient: "))
+b = int(input("Enter a value for the b coeficient: "))
+c = int(input("Enter a value for the c coeficient: "))
+print("Your equation is " + str(a) + "x^2 + " + str(b) + "x - " + str(c))
+x1 = (-b + math.sqrt((b**2 - (4*a*c))))/(2*a)
+x2 = (-b - math.sqrt((b**2 - (4*a*c))))/(2*a)
+print("The roots of this quadratic equation are x = " + str(x1) + " and x = " + str(x2))
