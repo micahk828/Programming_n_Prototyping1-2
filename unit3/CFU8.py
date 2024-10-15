bbe6671c862054ad3d3ca81eaa738ed33a21b59e
Micah Kennedy <144722193+micahk828@users.noreply.github.com>
2024-10-15 08:23:52 -0400

    Create CFU8.py

---
+++ unit3/CFU8.py
@@ -0,0 +1,20 @@
+#Micah Kennedy
+#10/15/2024
+#Pd1-2
+
+#version 1
+deli = input("Did you order delivery? ")
+if deli == "yes" or "Yes":
+    print("Great!")
+else:
+    print("No?! So who is cooking?")
+#version 2    
+deli = input("Did you order delivery? ")
+if deli == "yes" or "Yes":
+    print("Great!")
+    cost = float(input("What is the cost of the food ordered? "))
+    people = int(input("How many people are splitting the bill? "))
+    cpp = cost/people
+    print("The cost per person is $" + str(cpp))
+else:
+    print("No?! So who is cooking?")
