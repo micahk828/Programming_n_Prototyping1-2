8a30270cb7e34a66b1fc8e9ffbd6250aeed965eb
Micah Kennedy <144722193+micahk828@users.noreply.github.com>
2024-10-31 12:02:57 -0700

    Update CFU12.py

--- unit3/CFU12.py
+++ unit3/CFU12.py
@@ -2,23 +2,33 @@
 #10/29/2024
 #Pd 1-2
 
-'''
-Version 1
-entry = input("Enter your password: ")
-while entry != "simonnyc":
-    print("Wrong Password")
-    entry = input("Enter your password: ")
-print("Correct! You may enter")
-'''
 
-#Version 2
-entry = input("Enter your password: ")
-attempts=1
-while entry != "simonnyc" and attempts < 3:
-    print("Wrong Password")
-    attempts += 1
+def vers1():
     entry = input("Enter your password: ")
-    if attempts >= 3:
-        print("Too many bad attempts")
-if entry == "simonnyc": 
-    print("Correct! You may enter -->")
+    while entry != "simonnyc":
+        print("Wrong Password")
+        entry = input("Enter your password: ")
+    print("Correct! You may enter")
+
+
+def vers2():
+    entry = input("Enter your password: ")
+    attempts=1
+    while entry != "simonnyc" and attempts < 3:
+        print("Wrong Password")
+        attempts += 1
+        entry = input("Enter your password: ")
+        if attempts >= 3:
+            print("Too many bad attempts")
+    if entry == "simonnyc": 
+        print("Correct! You may enter -->")
+
+def main():
+    vers = int(input("Enter 1 for Version 1 or 2 for Version 2: "))
+    if vers == 1:
+        vers1()
+    elif vers == 2:
+        vers2()
+    else:
+        main()
+main()
