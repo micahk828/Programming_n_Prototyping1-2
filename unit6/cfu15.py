6fe04c323681a113f045932d64213b6643a7748b
Micah Kennedy <144722193+micahk828@users.noreply.github.com>
2024-11-28 13:49:45 -0500

    Update cfu15.py

--- unit6/cfu15.py
+++ unit6/cfu15.py
@@ -6,18 +6,18 @@ import simplegui
 
 def draw_handler(canvas):
     # Face 
-    canvas.draw_polygon([(50, 100), (70, 50), (120, 30), (170, 50), (190, 100), (170, 150), (120, 170), (70, 150)], 2, "white")
+    canvas.draw_polygon([(35, 100), (55, 50), (105, 30), (155, 50), (175, 100), (155, 150), (105, 170), (55, 150)], 2, "white")
     
     # Left Eye
-    canvas.draw_polygon([(90, 80), (100, 80), (100, 90), (90, 90)], 2, "white")
+    canvas.draw_polygon([(75, 80), (85, 80), (85, 90), (75, 90)], 2, "white")
     
     # Right Eye
-    canvas.draw_polygon([(140, 80), (150, 80), (150, 90), (140, 90)], 2, "white")
+    canvas.draw_polygon([(125, 80), (135, 80), (135, 90), (125, 90)], 2, "white")
     
     # Smile
-    canvas.draw_polygon([(80, 130), (120, 150), (160, 130)], 2, "white")
+    canvas.draw_polygon([(65, 130), (105, 150), (145, 130)], 2, "white")
 
-frame = simplegui.create_frame("CFU 15: Happy Shapes", 250, 200)
+frame = simplegui.create_frame("CFU 15: Happy Shapes", 200, 200)
 frame.set_canvas_background("black")
 frame.set_draw_handler(draw_handler)
 frame.start()
